610c96720ad7ff43a95087200514a3ec1ee46e23
Venkat <vvedhadri@gmail.com>
2026-07-09 11:55:12 +0530

    Update rag_pipeline_V2.py

--- rag_pipeline_V2.py
+++ rag_pipeline_V2.py
@@ -4,19 +4,27 @@ import chromadb
 from langchain_groq import ChatGroq
 import os 
 import streamlit as st
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+
+embedding_function = SentenceTransformerEmbeddingFunction(
+    model_name="all-MiniLM-L6-v2"
+)
 
 api_key = st.secrets["GROQ"]
 llm = ChatGroq(
     model= "llama-3.3-70b-versatile",
     api_key=api_key,
     temperature=0.3)
-client = chromadb.EphemeralClient()
+client = chromadb.PersistentClient(path="./chroma_db")
 
 def process_pdf(pdf_path,name):
     
     existing = [c.name for c in client.list_collections()]
     if name in existing:
-        collection = client.get_or_create_collection(name=name)
+        collection = client.get_collection(
+        name=name,
+        embedding_function=embedding_function
+    )
         return collection.count()
     # Load, chunk, embed, store in ChromaDB
     splitter = RecursiveCharacterTextSplitter(chunk_size=2000,chunk_overlap=300)
@@ -25,7 +33,10 @@ def process_pdf(pdf_path,name):
     pages = loader.load()
     chunks = splitter.split_documents(pages)
     
-    collection = client.get_or_create_collection(name=name)
+    collection = client.get_or_create_collection(
+    name=name,
+    embedding_function=embedding_function
+)
     collection.add(
         documents=[chunk.page_content for chunk in chunks],
         ids = [f"chunk_{i+1}" for i in range(len(chunks))],
@@ -36,7 +47,10 @@ def process_pdf(pdf_path,name):
     # Returns: number of chunks stored
 
 def ask_question(question,collection_name):
-    collection = client.get_or_create_collection(collection_name)
+    collection = client.get_or_create_collection(
+    name=collection_name,
+    embedding_function=embedding_function
+)
     results = collection.query(
         query_texts=[question],
         n_results=3
